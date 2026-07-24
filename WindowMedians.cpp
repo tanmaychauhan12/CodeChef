13dfa05ded87b75d044846f97db059c02250aa25
Tanmay Chauhan <chauhantanmay253@gmail.com>
2026-07-24 16:53:43 +0530

    Create WindowMedians.cpp

---
+++ WindowMedians.cpp
@@ -0,0 +1,15 @@
+// Problem : https://www.codechef.com/problems/SBMD
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n,k;
+  cin >> n >> k;
+  vector<int> nums(n);
+  for(int i=0;i<n;i++){
+    cin >> nums[i];
+  }
+  
+  return 0;
+}
