25834157e58da505af7a85a20435c15296f9eaa4
Tanmay Chauhan <chauhantanmay253@gmail.com>
2026-07-26 00:38:47 +0530

    Create TheCateringChallenge.cpp

---
+++ TheCateringChallenge.cpp
@@ -0,0 +1,18 @@
+// Problem : https://www.codechef.com/problems/CTRCH
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,d;
+    cin >> n >> d;
+
+    return;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
