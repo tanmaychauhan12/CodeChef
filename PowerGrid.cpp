8e16ccdad21db13a7845f0ac5492dbbb2dd093b3
Tanmay Chauhan <chauhantanmay253@gmail.com>
2026-07-25 16:41:19 +0530

    Create PowerGrid.cpp

---
+++ PowerGrid.cpp
@@ -0,0 +1,11 @@
+// Problem : https://www.codechef.com/problems/POWERGRID
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r,c,w;
+    cin >> r >> c >> w;
+    cout << r * c * w;
+    return 0;
+}
