dc77e7eb30e4a254c991b0de8da3faf57b241a8e
Tanmay Chauhan <chauhantanmay253@gmail.com>
2026-07-27 00:06:28 +0530

    Create CriticsSpecial.cpp

---
+++ CriticsSpecial.cpp
@@ -0,0 +1,14 @@
+// Problem : https://www.codechef.com/problems/CRTCS
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+  
+}
+
+int main(){
+  int t;
+  cin >> t;
+  while(t--) solve();
+}
