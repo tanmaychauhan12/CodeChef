fffdd53a6bdabfa99ecc5956ac3beb648d2b3445
Tanmay Chauhan <chauhantanmay253@gmail.com>
2026-07-27 16:37:11 +0530

    Create CountSubarrays.cpp

---
+++ CountSubarrays.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for(int i=0;i<n;i++){
+        cin >> nums[i];
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
